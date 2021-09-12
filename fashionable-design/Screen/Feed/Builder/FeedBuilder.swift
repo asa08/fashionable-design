779c30befe2a0fbceae909d16793b5ebe6b89dd0
maiko-m <mn.6182.nm@gmail.com>
2021-09-12 21:45:50 +0900

    :sparkles: init

---
+++ fashionable-design/Screen/Feed/Builder/FeedBuilder.swift
@@ -0,0 +1,15 @@
+//
+//  FeedBundler.swift
+//  fashionable-design
+//
+//  Created by maiko morisaki on 2021/09/12.
+//
+
+import UIKit
+
+class FeedBuilder {
+    func build() -> UIViewController {
+        let controller = FeedViewController()
+        return controller
+    }
+}
---
+++ fashionable-design/Screen/Feed/View/FeedViewController.swift
@@ -0,0 +1,12 @@
+//
+//  FeedViewController.swift
+//  fashionable-design
+//
+//  Created by maiko morisaki on 2021/09/12.
+//
+
+import UIKit
+
+class FeedViewController: UIViewController {
+    
+}
